d58d08e6ea478851e6b2f263b0ce2a0cb2d6b376
Julia Rosemary Thorpe <jrth@win.dtu.dk>
2018-07-20 10:58:16 +0200

    Changed action range calculation to max distance from home for both stays and moves (instead of max for move and centroid for stays)

--- Rscripts/jrt_mobility.R
+++ Rscripts/jrt_mobility.R
@@ -244,7 +244,7 @@ summarise_trajectories <- function(gps.traj, dist.threshold) {
     summarize(T.start = timestamp[1], T.end = timestamp[length(timestamp)], 
               is.stay = median(is.stay), loc.id = mean(loc.id),
               clat = ifelse(is.stay, mean(lat), NA), clon = ifelse(is.stay, mean(lon), NA),    # stays centroids
-              action.range = ifelse(is.stay,mean(homedist,na.rm=TRUE),max(homedist,na.rm=TRUE))# distance from home (furthest on move, average in stay)
+              action.range = max(homedist,na.rm=TRUE)# distance from home (furthest on move, average in stay)
     ) %>%
     mutate(T.end = c(T.start[-1],T.end[length(T.end)])) %>%
     mutate(durations = difftime(T.end,T.start, units = "mins")) %>%
