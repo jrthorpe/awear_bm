39395cfa3a7a9f90cc1cc77120e677c61c23c952
Julia Rosemary Thorpe <jrth@win.dtu.dk>
2018-07-31 11:26:29 +0200

    Evaluation / Assessments / MobilityZones: All in as a chunk in the main Rmd script. Created a style sheet to reuse for other plots.

---
+++ Rscripts/stylesheet.R
@@ -0,0 +1,13 @@
+# STYLE SHEET
+
+# annotations for subplot titles
+anno_subtitle <- list(
+  #text = paste0("P",i), # define custom per plot
+  xref = "paper",
+  yref = "paper",
+  yanchor = "bottom",
+  xanchor = "center",
+  align = "center",
+  x = 0.5,
+  y = 1,
+  showarrow = FALSE)
