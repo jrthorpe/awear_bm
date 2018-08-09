0aa3bf0557a7469a78c9e16cfd2488b9fa971174
Julia Rosemary Thorpe <jrth@win.dtu.dk>
2018-08-09 11:19:31 +0200

    Started fixing up plots: uses time axis with defined range and intervals, as specified in "stylesheet" file.

--- Rscripts/stylesheet.R
+++ Rscripts/stylesheet.R
@@ -1,5 +1,8 @@
 # STYLE SHEET
 
+colourset <- brewer.pal(9, "Set1")
+colours.compare <- brewer.pal(3, "Set2")
+
 # annotations for subplot titles
 anno_subtitle <- list(
   #text = paste0("P",i), # define custom per plot
@@ -11,3 +14,25 @@ anno_subtitle <- list(
   x = 0.5,
   y = 1,
   showarrow = FALSE)
+
+timeaxis <- list(
+  title = " ",
+  autotick = FALSE,
+  ticks = "outside",
+  #tick0 = 0,
+  dtick = 86400000/48, #86400000 corresponds to 1 day
+  ticklen = 5,
+  tickwidth = 2#,
+  #range = c(2,5)#,
+  #tickcolor = toRGB("blue")
+)
+
+# # example for experimentation
+# plot_ly(stepcounters.p %>% filter(dates==d) %>% group_by(source),
+#         x = ~timestamp, y = ~stepcounter,
+#         type = "scatter", mode = "lines+markers",
+#         color = ~source, colors = colourset[4:5],
+#         legendgroup = "Steps", name = "source")  %>%
+#   layout(yaxis=list(title="Step count"),
+#          xaxis = timeaxis,
+#          margin = list(l = 50, r = 50, b = 50, t = 50, pad = 4))
