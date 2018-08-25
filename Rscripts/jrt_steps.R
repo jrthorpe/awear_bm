eb95e5b9e8634d5abdecfa18037285094390de05
Julia Thorpe <Julia Thorpe>
2018-08-25 16:55:07 +0200

    Changed the way step logs are restructured into daily cumulative steps to overcome the issue where the stepcount is oscilating. Instead of removing  negative step increments, remove all step increments above a certain threshold in either direction.

--- Rscripts/jrt_steps.R
+++ Rscripts/jrt_steps.R
@@ -10,8 +10,12 @@ daily_steps <- function(steps.log, device){
              tail(timestamp, -1), head(timestamp, -1), units = "mins"
            )))
   
-  # Remove drops in steps (caused by reboot) #note: some small drops appear to happen without reboot, see issues
-  steps$dstep[steps$dstep<0] <- 0
+  # # Remove drops in steps (caused by reboot) #note: some small drops appear to happen without reboot, see issues
+  # steps$dstep[steps$dstep<0] <- 0
+  
+  # Instead of removing drops in steps, remove drastic changes according to threshold (set the differences in steps to 0):
+  T.steps <- 10000 #threshold for increments in steps
+  steps$dstep[abs(steps$dstep)>T.steps] <- 0
   
   # cummulate over day instead of between reboots
   steps %<>% group_by(dates) %>% mutate(stepcounter = cumsum(dstep))
@@ -28,4 +32,24 @@ pattern_steps <- function(steps, win.size){
   
   return(win.totals)
 
-  }
+}
+
+
+# # TESTING & DEBUGGING
+# steps.log %<>% arrange(dsource) %>% mutate(dSdT = abs(c(0,diff(step_count))))
+# 
+# plot_ly(data = steps.log %>% group_by(dsource),
+#         x = ~timestamp,
+#         y = ~step_count,
+#         color = ~dsource,
+#         colors = colourset[1:2],
+#         type = "scatter",
+#         mode = "markers+lines") %>%
+#   add_trace(data = steps.log %>% group_by(dsource),
+#             x = ~timestamp,
+#             y = ~dSdT,
+#             inherit = F,
+#             type = "scatter",
+#             mode = "markers+lines",
+#             color = ~dsource,
+#             colors = colourset[3:4])
