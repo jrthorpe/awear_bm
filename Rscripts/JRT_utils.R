17e367bba5ac0a7b4e54307d061be3ef209433e3
Julia Thorpe <Julia Thorpe>
2018-08-26 15:17:02 +0200

    small edit to make sure NA's don't mess with the score converter

--- Rscripts/JRT_utils.R
+++ Rscripts/JRT_utils.R
@@ -446,7 +446,7 @@ mutate_cond <- function(.data, condition, ..., envir = parent.frame()) {
 
 score_converter <- function(metric, N){
   
-  Q <- quantile(metric, probs = c(1:N)/N)
+  Q <- quantile(metric, probs = c(1:N)/N, na.rm = T)
   scores <- cut(metric,
                 breaks = c(-1,Q) %>% as.numeric(), #put -1 instead of 0 incase the first Q is zero. No metrics have negative values, therefore no difference between -1:Q1 range and 0:Q1 range.
                 labels = c(1:N))
